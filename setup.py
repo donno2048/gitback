cf9e3946c8d5b2799734752d7dff4b63103e58a0
Elisha Hollander <just4now666666@gmail.com>
2021-02-16 18:31:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 setup(
     name='gitback',
-    version='1.0.2',
+    version='1.0.3',
     description='Makes a backup of all your repositories and gists from GitHub',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
@@ -10,5 +10,6 @@ setup(
     license='MIT',
     author='Elisha Hollander',
     classifiers=['Programming Language :: Python :: 3'],
-    install_requires=['PyGithub']
+    install_requires=['PyGithub'],
+    entry_points={ 'console_scripts': [ 'gitback=gitback.__init__:backup' ] }
 )
