ebea57585fef9eac68b2b388d8d81749b29816ec
Elisha Hollander <just4now666666@gmail.com>
2021-04-11 01:09:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,5 @@ setup(
     license='MIT',
     author='Elisha Hollander',
     classifiers=['Programming Language :: Python :: 3'],
-    install_requires=['PyGithub'],
     entry_points={ 'console_scripts': [ 'gitback=gitback.__init__:backup' ] }
 )
