99c0be98bccab8c35330588c30d9701d29091168
Elisha Hollander <just4now666666@gmail.com>
2021-04-11 01:08:44 +0300

    Update __init__.py

--- gitback/__init__.py
+++ gitback/__init__.py
@@ -1,8 +1,9 @@
-from github import Github
 from os import system, chdir, mkdir, getcwd
 from os.path import exists
 from zipfile import ZipFile
 from argparse import ArgumentParser
+from six.moves.urllib.request import urlopen
+from json import loads
 def backup()->None:
     parser = ArgumentParser(description='Makes a backup of all your repositories and gists from GitHub')
     parser.add_argument('-n', '--name', metavar='', type=str, help='Use a custom name for your backup [default: backup]')
@@ -24,16 +25,16 @@ def backup()->None:
     try: mkdir(name)
     except: exit(f'You already have a {name} folder, please move it somewhere else until the process is done')
     chdir(name)
-    me, git_command = Github(args.username if args.username is not None else input("Your GitHub username: "), args.password if args.password is not None else input("Your GitHub password: ")).get_user(), 'git clone '
+    username, git_command = args.username if args.username is not None else input("Your GitHub username: "), 'git clone '
     if not args.full: git_command += '--depth 1 '
     if args.quiet: git_command += '-q '
     repo_clone_command = 'https://github.com/' if not args.ssh else 'git@github.com:'
     gist_clone_command = 'https://gist.github.com/' if not args.ssh else 'git@gist.github.com:'
     if not args.repos:
-        for gist in me.get_gists():
+        for gist in loads(urlopen(f"https://api.github.com/users/donno2048/gists?type=all&per_page=1000").read()):
             system(git_command + gist_clone_command + gist.id)
             if args.zip: backup.write(getcwd() + '\\' + gist.id, arcname = gist.id)
     if not args.gists:
-        for repo in me.get_repos():
+        for repo in loads(urlopen(f"https://api.github.com/users/donno2048/repos?type=all&per_page=1000").read()):
             system(git_command + repo_clone_command + repo.full_name)
             if args.zip: backup.write(getcwd() + '\\' + repo.name, arcname = repo.name)
