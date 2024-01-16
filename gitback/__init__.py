215be526269661fd89bbe514e94fd27c6a5bc062
Elisha Hollander <just4now666666@gmail.com>
2024-01-17 00:05:13 +0200

    use `shutil` to zip

--- gitback/__init__.py
+++ gitback/__init__.py
@@ -1,7 +1,7 @@
 from os import chdir, mkdir, rmdir, getcwd
 from os.path import exists
 from subprocess import Popen, PIPE
-from zipfile import ZipFile
+from shutil import make_archive
 from argparse import ArgumentParser
 from urllib.request import urlopen
 from json import loads
@@ -21,8 +21,7 @@ def backup()->None:
     if args.path is not None: chdir(args.path)
     username, git_command = args.username if args.username is not None else input("Your GitHub username: "), ' clone '
     name = 'backup' if args.name is None else args.name
-    if exists(name+'.zip') and args.zip: exit(f'You already have a {name} zip, please move it somewhere else until the process is done')
-    if args.zip: backup = ZipFile(name+'.zip', 'w', 8)
+    if exists(name + '.zip') and args.zip: exit(f'You already have a {name} zip, please move it somewhere else until the process is done')
     try: mkdir(name)
     except FileExistsError:
         try:
@@ -39,12 +38,12 @@ def backup()->None:
         while len(result := loads(urlopen(f"https://api.github.com/users/{username}/gists?type=all&per_page=100&page={i}").read())):
             for gist in result:
                 Popen('git' + git_command + gist_clone_command + gist["id"], shell = True, stdout = PIPE).wait()
-                if args.zip: backup.write(getcwd() + '/' + gist["id"], arcname = gist["id"])
             i += 1
     if not args.gists:
         i = 0
         while len(result := loads(urlopen(f"https://api.github.com/users/{username}/repos?type=all&per_page=100&page={i}").read())):
             for repo in result:
                 Popen('git' + git_command + repo_clone_command + repo["full_name"], shell = True, stdout = PIPE).wait()
-                if args.zip: backup.write(getcwd() + '/' + repo["name"], arcname = repo["name"])
             i += 1
+    chdir('..')
+    make_archive(name + '.zip', 'zip', name)
